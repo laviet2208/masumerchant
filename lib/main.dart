58bdd4c1d5002fa77df797978dfab82b8d9c6e84
tuanp <tuanp@DESKTOP-JRKGVU0>
2024-03-17 23:30:47 +0700

    first release

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:masumerchant/SCREEN/Tr%C6%B0%E1%BB%9Bc%20khi%20v%C3%A0o%20app/ScreenLogin.dart';
+
+
+Future<void> main() async
+{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Xeko merchant',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const ScreenLogin(),
+    );
+  }
+}
